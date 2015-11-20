261d1e0f5e715eb8ee1bed4de1e83958bb70c1c5
Gianmarco Nalin <gianmarconalin@MacBook-Pro-di-Andrea-2.local>
2015-11-20 16:39:13 +0100

    Bootstrap added.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Kargaroo</title>
+        <title>Join the Community :: Kargaroo</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 	</body>
